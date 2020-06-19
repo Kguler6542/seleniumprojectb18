226f71a5c88094833d98c31512c4612488c3b3b7
Kguler6542 <Kamuranguler2017@gmail.com>
2020-06-19 17:01:01 -0400

    commit

---
+++ src/test/java/test/day6_testng_dropdowns/TestNGPractice.java
@@ -0,0 +1,52 @@
+package test.day6_testng_dropdowns;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNGPractice {
+
+    @BeforeClass
+    public void before_class(){
+        System.out.println("BeforeCLASS is running! ----------------");
+    }
+
+    @AfterClass
+    public void after_class(){
+        System.out.println("AfterCLASS is running! -----------------");
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Before method is running...");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("After method is running...");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running...");
+
+        String actualTitle = "Homepage"; //driver.getTitle();
+        String expectedTitle = "Homepage";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+//        if(actualTitle.equals(expectedTitle)){
+//            System.out.println("Title verification passed!");
+//        }else{
+//            System.out.println("Title verification failed!!!");
+//        }
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 is running...");
+    }
+
+
+
+
+}
--- src/test/java/utilities/Test.java
+++ src/test/java/utilities/Test.java
@@ -1,43 +1,20 @@
 package utilities;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
 
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate")); // Outputs 7
 
-        //TC	#2:	SeleniumEasy	CheckboxVerification–Section	1
-        // 1.Open	Chrome	browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
-        // WebDriver driver = WebDriverFactorry.get
-        driver.manage().window().maximize();
-        //driver.manage.win.max
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName.concat(lastName));
 
-        // 2.Go	to	https://www.seleniumeasy.com/test/basic-checkbox-demo.html
-        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
-        //driver. get
-
-        // 3.Verify	“Success	–Check	box	is	checked”	message	is	NOTdisplayed.
-
-        WebElement succCb= driver.findElement(By.xpath("//input[@id='isAgeSelected']"));
-        WebElement sucM = driver.findElement(By.xpath("//div[@id='txtAge']"));
-        //WebElemenet x= driver.findElement
-
-        // 4.Click	to	checkbox	under	“Single	Checkbox	Demo”	section
-        Thread.sleep(2000);
-         succCb.click();
+        System.out.println(firstName+" "+lastName);
 
 
-        // 5.Verify	“Success	–Check	box	is	checked”	message	isdisplayed
-
-        if(sucM.isDisplayed()){
-            System.out.println("pass");
-        }else
-            System.out.println("not pas");
-
-        driver.quit();
     }
 }
